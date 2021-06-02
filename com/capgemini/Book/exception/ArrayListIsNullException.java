fdcf201192bff4a7a54682437891b7407371b1e4
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-06-02 09:56:57 +0530

    Create ArrayListIsNullException.java

---
+++ com/capgemini/Book/exception/ArrayListIsNullException.java
@@ -0,0 +1,8 @@
+package com.capgemini.Book.exception;
+
+@SuppressWarnings("serial")
+public class ArrayListIsNullException extends Exception {
+	public ArrayListIsNullException(String msg){
+		super(msg);
+	}
+}
